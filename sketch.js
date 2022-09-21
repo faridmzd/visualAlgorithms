f090f80e6d1fbfce6dd71fbc73fde0bba9a49997
Farid <66147811+feridmzd@users.noreply.github.com>
2022-09-22 03:19:23 +0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,22 @@
 let values;
-let n;
-let i = 0;
+let n,i = 0;
+var _height,_width,_body,_html;
+
 function setup() {
-  createCanvas(400, 400);
-  values = new Array(width);
+   _body = document.body;
+   _html = document.documentElement;
+
+   _height = Math.max( body.scrollHeight, body.offsetHeight, 
+                       html.clientHeight, html.scrollHeight, html.offsetHeight );
+  
+   _width = Math.max( body.scrollWidth, body.offsetWidth, 
+                       html.clientWidth, html.scrollWidth, html.offsetWidth );  
+
+  createCanvas(_width,_height);
+  
+  values = new Array(_width);
   for(let i = 0; i < values.length; i++){
-    values[i] = randomInteger(0,height);
+    values[i] = randomInteger(0,_height);
   }
   n = values.length;
 }
@@ -33,7 +44,7 @@ function draw() {
     for (let i = 0; i < n - 1; i++){
                      stroke(255, 204, 0);
                   
-                    line(i,height,i,height-values[i]);
+                    line(i,_height,i,_height-values[i]);
     }
 }
   
